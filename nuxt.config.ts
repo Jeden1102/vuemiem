97d70961c3e52c7ee72ee9923eaff99446d2bd23
Jeden1102 <bum123@o2.pl>
2024-11-03 17:41:25 +0100

    Init project

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [[
+    "@nuxtjs/google-fonts",
+    {
+      families: {
+        Inter: "100..900",
+        "Kumbh Sans": "100..900",
+      },
+    },
+  ], "@nuxtjs/tailwindcss"],
+});
