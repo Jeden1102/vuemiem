7cc7a41e2f1c51e3d3d041c3d24296a0349f9d54
Jeden1102 <43098219+Jeden1102@users.noreply.github.com>
2025-01-02 12:50:35 +0100

    master. Chaned per page blogs amount.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,7 +4,7 @@
       title="Zanurz się w świecie frontendu"
       description="Odkryj nasze artykuły o Vue, Nuxt i technologiach webowych. Poznaj najnowsze trendy, praktyczne poradniki i nowinki ze świata frontend developmentu. Bez względu na Twój poziom zaawansowania, znajdziesz tu inspirację i wiedzę, która przyspieszy Twój rozwój."
     />
-    <BlogViewTeaserSimple :perPage="5" :isPaginated="true" :grid="true" />
+    <BlogViewTeaserSimple :perPage="12" :isPaginated="true" :grid="true" />
   </div>
 </template>
 
